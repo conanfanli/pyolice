ce2745b3604ecd0430a45f57814ef3761e93046a
Conan Li <conanlics@gmail.com>
2020-11-17 19:25:16 -0500

    rename variables

--- pyolice/command_line.py
+++ pyolice/command_line.py
@@ -8,27 +8,27 @@ def print_warning_message(message: str) -> None:
 
 
 @dataclass
-class Violation:
+class Crime:
     pattern: str  # Search pattern
     directory: str  # directory where the search should take place
-    message: str  # Message to display when a violation is found
+    message: str  # Message to display when a crimes is found
     excluded_files: List[str]  # Files to be excluded from the search
 
 
-violations = [
-    Violation(
+CRIMES = [
+    Crime(
         pattern="UserLogin.objects.create",
         directory="apps",
         message="Should not directly ORM to create UserLogin. Use UserAnalyticsService instead.",
         excluded_files=["apps/analytics/backends.py"],
     ),
-    Violation(
+    Crime(
         pattern="UserRegister.objects.create",
         directory="apps",
         message="Should not directly ORM to create UserRegister. Use UserAnalyticsService instead.",
         excluded_files=["apps/analytics/backends.py"],
     ),
-    Violation(
+    Crime(
         pattern="UserEmail.objects.get",
         directory="apps",
         message="Should not directly ORM to get UserEmail. Use UserEmailService.get_by_email instead.",
@@ -37,20 +37,23 @@ violations = [
             "apps/wave_auth/tests/models_tests.py",
         ],
     ),
-    Violation(
+    Crime(
         pattern="Site.objects",
         directory="apps",
-        message="The `sites` framework is not needed for retrieving the current domain. Check out the functions in 'core.utils' for your need.",
+        message=(
+            "The `sites` framework is not needed for retrieving the current domain."
+            "Check out the functions in 'core.utils' for your need."
+        ),
         excluded_files=["apps/wave_auth/migrations/0003_default_fixtures.py"],
     ),
 ]
 
 
-def check_violations() -> bool:
+def detect_crimes() -> bool:
     success = True
-    for violation in violations:
+    for crime in CRIMES:
         result = subprocess.run(
-            f"ag {violation.pattern} {violation.directory} -l",
+            f"ag {crime.pattern} {crime.directory} -l",
             shell=True,
             capture_output=True,
         )
@@ -58,13 +61,11 @@ def check_violations() -> bool:
         if result.returncode == 0:
             matched_files = result.stdout.decode("utf-8").strip().split("\n")
             violation_files = sorted(
-                set(matched_files) - set(violation.excluded_files or [])
+                set(matched_files) - set(crime.excluded_files or [])
             )
             if violation_files:
-                print(
-                    f"Found violation pattern '{violation.pattern}' in {violation_files}"
-                )
-                print_warning_message(violation.message)
+                print(f"Found crime pattern '{crime.pattern}' in {violation_files}")
+                print_warning_message(crime.message)
                 print()
                 success = False
 
@@ -72,5 +73,5 @@ def check_violations() -> bool:
 
 
 def main() -> int:
-    success = check_violations()
+    success = detect_crimes()
     return 0 if success else 1
